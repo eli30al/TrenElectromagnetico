d07b42630a7b50bc0494b4ddf49a0375585bf383
Juan Carlos Lopez <jclopez@udlanet.ec>
2017-01-07 16:14:23 -0500

    Clase hora, clase vuelta
    constructor en las clases, get y set

--- TrenElectromagnetico/src/trenelectromagnetico/Prueba.java
+++ TrenElectromagnetico/src/trenelectromagnetico/Prueba.java
@@ -2,12 +2,5 @@
 package trenelectromagnetico;
 
 public class Prueba {
-    private String ruta;
-    private int pasajeros;
-    private float velocidad;
-    private float tiempo;
-    private int codigo;
-    private Hora llegada;
-    private Hora salida;
     
 }
--- TrenElectromagnetico/src/trenelectromagnetico/Vuelta.java
+++ TrenElectromagnetico/src/trenelectromagnetico/Vuelta.java
@@ -1,14 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package trenelectromagnetico;
 
-/**
- *
- * @author Elisabet Alvarez
- */
 public class Vuelta {
+    private String ruta;
+    private int pasajeros;
+    private float velocidad;
+    private float tiempo;
+    private int codigo;
+    private Hora llegada;
+    private Hora salida;
+
+    public Vuelta(String ruta, int pasajeros, float velocidad, float tiempo, int codigo, Hora llegada, Hora salida) {
+        this.ruta = ruta;
+        this.pasajeros = pasajeros;
+        this.velocidad = velocidad;
+        this.tiempo = tiempo;
+        this.codigo = codigo;
+        this.llegada = llegada;
+        this.salida = salida;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public int getPasajeros() {
+        return pasajeros;
+    }
+
+    public void setPasajeros(int pasajeros) {
+        this.pasajeros = pasajeros;
+    }
+
+    public float getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(float velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public float getTiempo() {
+        return tiempo;
+    }
+
+    public void setTiempo(float tiempo) {
+        this.tiempo = tiempo;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public Hora getLlegada() {
+        return llegada;
+    }
+
+    public void setLlegada(Hora llegada) {
+        this.llegada = llegada;
+    }
+
+    public Hora getSalida() {
+        return salida;
+    }
+
+    public void setSalida(Hora salida) {
+        this.salida = salida;
+    }
+    
     
 }
