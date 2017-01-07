6f3e8735cea86eccae5e9d6836b87fa1f91126b8
eli30al <zaida.alvarez@udlanet.ec>
2017-01-07 17:40:35 -0500

    Hora

--- TrenElectromagnetico/src/trenelectromagnetico/Hora.java
+++ TrenElectromagnetico/src/trenelectromagnetico/Hora.java
@@ -1,6 +1,8 @@
 
 package trenelectromagnetico;
 
+import java.util.StringTokenizer;
+
 public class Hora {
     private int hora;
     private int min;
@@ -34,6 +36,35 @@ public class Hora {
 
     public void setSeg(int seg) {
         this.seg = seg;
+    }		
+    public Hora(){}
+    
+    public Hora(String hora){
+      StringTokenizer st= new StringTokenizer(hora,":");  
+      Hora aux = new Hora();
+      String palabra=st.nextToken();
+            if(palabra!=null&&st.hasMoreTokens()){//COMPARO SI PUEDO SEGUIR AÑADIENDO
+                aux.setHora(Integer.parseInt(palabra));
+            }else{return;}
+            palabra=st.nextToken();
+            if(palabra!=null&&st.hasMoreTokens()){
+                aux.setMin(Integer.parseInt(palabra));
+            }else{return;}
+            palabra=st.nextToken();
+            if(palabra!=null){ //VALIDAMOS EL TIPO DE PERSONA
+                aux.setSeg(Integer.parseInt(palabra));
+            }else{return;}
     }
     
+    public String toString(){
+            String c="",d="",e="";
+            c= Integer.toString(hora);
+            d=Integer.toString(min);
+            e=Integer.toString(seg);
+            if(hora<10)c= "0"+hora;
+            if(min<10)d= "0"+min;
+            if(seg<10)e= "0"+seg;
+             return c+":"+d+":"+e;
+    }
+
 }
